25dd5b06df9d7488af10a492eb84fa3478ff7732
Javier Portilla Rosero <javierportillarosero@MacBook-Pro-de-Javier.local>
2024-01-29 12:40:38 -0500

    Text categories changed

---
+++ src/components/ProductsCategories.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const ProductsCategories = () => {
+  return (
+    <div className='products__categories'>
+        <ul>
+          <li><a href="">Aseo Personal</a></li>
+          <li><a href="">Salud</a></li>
+          <li><a href="">Aseo General</a></li>
+          <li><a href="">Canasta Familiar</a></li>
+          <li><a href="">Snacks_Mecato</a></li>
+          <li><a href="">Sobre Pedidos (Electromenores)</a></li>
+          <li><a href="">Hogar</a></li>
+          <li><a href="">Mascotas</a></li>
+        </ul>
+      </div>
+  )
+}
