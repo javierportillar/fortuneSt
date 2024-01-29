71c5d439e176bb3ffe069ceb99956433d203a03e
Javier Portilla Rosero <javierportillarosero@MacBook-Pro-de-Javier.local>
2024-01-29 11:26:05 -0500

    App.jsx updated for Routes Managment

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import { useState } from 'react'
-import { Nav } from './components/nav'
-import { MainLanding } from './components/mainLanding'
-import './App.css'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Nav } from './components/nav';
+import { MainLanding } from './components/mainLanding';
+import {ProductsPage} from './components/ProductsPage';
+import {ServicesPage} from './components/ServicesPage';
+import './App.css';
 
 function App() {
-
   return (
-    <div>
-      <Nav></Nav>
-      <MainLanding></MainLanding>
-    </div>
-  )
+    <Router>
+      <Nav />
+      <Routes>
+        <Route path="/services" element={<ServicesPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/" element={<MainLanding />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
