1ac6bab701083083962da2e83f594e7ab4b91328
Javier Portilla Rosero <javierportillarosero@MacBook-Pro-de-Javier.local>
2024-01-29 19:21:39 -0500

    removed extra ProductList from component

--- src/components/ProductsPage.jsx
+++ src/components/ProductsPage.jsx
@@ -1,22 +1,15 @@
 import { ProductsCategories } from "./ProductsCategories"
 import { ProductsList } from "./ProductsList"
 
+
 export const ProductsPage = () => {
+
+
   return (
     <div className='products'>
       <ProductsCategories></ProductsCategories>
         <h1>Productos destacados:</h1>
       <div className="main__products">
-        <ProductsList></ProductsList>
-        <ProductsList></ProductsList>
-        <ProductsList></ProductsList>
-        <ProductsList></ProductsList>
-        <ProductsList></ProductsList>
-        <ProductsList></ProductsList>
-        <ProductsList></ProductsList>
-        <ProductsList></ProductsList>
-        <ProductsList></ProductsList>
-        <ProductsList></ProductsList>
         <ProductsList></ProductsList>
       </div>
     </div>
