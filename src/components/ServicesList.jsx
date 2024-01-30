1364a48b36c3574ef7468620039379fb84a46bc8
Javier Portilla Rosero <javierportillarosero@MacBook-Pro-de-Javier.local>
2024-01-29 21:28:56 -0500

    Created new component for services list

---
+++ src/components/ServicesList.jsx
@@ -0,0 +1,13 @@
+
+export const ServicesList = () => {
+  return (
+    <div className='services__list'>
+      <ul>
+        <li>
+          <h3> Presta de dinero a bajo interés </h3>
+          <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, voluptatum. </p>
+        </li>
+      </ul>
+    </div>
+  )
+}
