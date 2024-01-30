9de3abc8389c63802906b44e6ed108386cc8d27e
Javier Portilla Rosero <javierportillarosero@MacBook-Pro-de-Javier.local>
2024-01-29 21:29:28 -0500

    Updated HTML code for different services

--- src/components/ServicesPage.jsx
+++ src/components/ServicesPage.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import { ServicesList } from "./ServicesList"
 
 export const ServicesPage = () => {
   return (
-    <div>ServicesPage</div>
+    <div className='services'>
+      <div className='services__tittle'>
+        <h2> A continuación se encuentran los servicios que Fondo Fortuna ofrece hasta el momento... </h2>
+      </div>
+      <div className='services__bg'>
+        <ServicesList></ServicesList>
+      </div>
+    </div>
   )
 }
